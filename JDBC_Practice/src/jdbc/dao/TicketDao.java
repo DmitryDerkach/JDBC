670086fc39fe446e7dca0ecb70c45f61601ec200
Dmitry <Dmitry@DESKTOP-3KI6QOB>
2021-11-04 10:47:55 +0300

    ...

--- JDBC_Practice/src/jdbc/dao/TicketDao.java
+++ JDBC_Practice/src/jdbc/dao/TicketDao.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import jdbc.dto.TicketFilter;
 import jdbc.entity.Flight;
 import jdbc.entity.Ticket;
